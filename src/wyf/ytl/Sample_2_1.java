07c1a09a4508506e058a41e5a7983d4acfa9571c
heigername <2523928151@qq.com>
2017-03-19 21:04:13 +0800

    test commit

---
+++ src/wyf/ytl/Sample_2_1.java
@@ -0,0 +1,13 @@
+package wyf.ytl;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Sample_2_1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_layout);
+	}
+}
